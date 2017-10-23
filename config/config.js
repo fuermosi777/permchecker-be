c9d7f641765315a8314c9de466566fc56f7630f5
fuermosi777 <liuhao1990@gmail.com>
2017-10-22 23:53:53 -0700

    rm prod logging

--- config/config.js
+++ config/config.js
@@ -14,6 +14,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: "127.0.0.1",
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: false
   }
 }
