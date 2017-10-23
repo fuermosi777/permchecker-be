3bac3afff0eda2dfb2ed8ff1c4b5bcbce5dcdf46
fuermosi777 <liuhao1990@gmail.com>
2017-10-22 19:20:36 -0700

    db

---
+++ config/config.js
@@ -0,0 +1,19 @@
+var process = require('process');
+
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "permchecker_dev",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "127.0.0.1",
+    dialect: "mysql"
+  }
+}
