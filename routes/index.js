7512b95f5a6c9a2bc673e7d267eaf0cadf9d0dca
Hao Liu <liuhao1990@gmail.com>
2017-12-27 14:11:08 -0800

    add stat on index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  let data = null;
+
+  try {
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    data = await axios.get(fullUrl + 'api/newapprovals');
+    data = data.data;
+  } catch (err) {
+    console.log('shit happens: approval data not acquired', err);
+  } finally {
+    res.render('index', { title: 'Express', data });
+  }
 });
 
 module.exports = router;
